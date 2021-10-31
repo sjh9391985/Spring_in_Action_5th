5569d56fc2a3fcb2b94a60c63fa7f9f8f3b3961d
JaeHyeon Son <sjh9391985@gmail.com>
2021-10-31 16:57:03 +0900

    Spring in Action: chapter3(Data develop)

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -1,11 +1,13 @@
 package tacos.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,24 +19,24 @@ import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
+import tacos.data.IngredientRepository;
 
 @Slf4j // Lombok에서 제공, 이 클래스에 자동으로 SLF4J Logger를 생성.
 @Controller // 해당 클래스를 찾은 후 스프링 어플리케이션 컨텍스트의 빈으로 글래스의 인스턴스를 자동 생성.
 @RequestMapping("/design") // 해당클래스가 처리하는 요청의 종류를 나타냄.
 public class DesignTacoController {
 
-	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-		// TODO Auto-generated method stub
-		return ingredients
-				.stream()
-				.filter(x -> x.getType().equals(type))
-				.collect(Collectors.toList());
+	private final IngredientRepository ingredientRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
 	}
 	
 	@GetMapping
 	public String showDesignForm(Model model) { // Model: 컨트롤러와 데이터를 보여주는 뷰 사이에서 데이터를 운반하는 객체.
 		
-		List<Ingredient> ingredients = Arrays.asList(
+		/*List<Ingredient> ingredients = Arrays.asList(
 				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
 				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
 				new Ingredient("GRBF", "Ground Beaf", Type.PROTEIN),
@@ -45,7 +47,10 @@ public class DesignTacoController {
 				new Ingredient("JACK", "Monterrey", Type.CHEESE),
 				new Ingredient("SLSA", "Salsa", Type.SAUCE),
 				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
+				);*/
+		
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 		
 		Type[] types = Ingredient.Type.values();
 		for(Type type : types) {
@@ -56,6 +61,14 @@ public class DesignTacoController {
 		
 		return "design";
 		
+	}
+	
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+		// TODO Auto-generated method stub
+		return ingredients
+				.stream()
+				.filter(x -> x.getType().equals(type))
+				.collect(Collectors.toList());
 	}
 	
 	@PostMapping
