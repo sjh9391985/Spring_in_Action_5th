e6b444377c464a03f79248cbe1bc55e37bcc730b
JaeHyeon Son <sjh9391985@gmail.com>
2021-11-02 09:41:31 +0900

    Spring in Action: chapter4(스프링 시큐리티 구성)

--- taco-cloud/src/main/java/tacos/web/WebConfig.java
+++ taco-cloud/src/main/java/tacos/web/WebConfig.java
@@ -1,19 +1,19 @@
-package tacos.web;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		// TODO Auto-generated method stub
-		
-		registry.addViewController("/").setViewName("home");
-
-	}
-	// 뷰 컨트롤러 역할을 수행하는 클래스.
-	
-}
+//package tacos.web;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		// TODO Auto-generated method stub
+//		
+//		registry.addViewController("/").setViewName("home");
+//
+//	}
+//	// 뷰 컨트롤러 역할을 수행하는 클래스.
+//	
+//}
